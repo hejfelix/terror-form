14e66f7879ffcc519cd3f614a2e1a306c662d3e3
Felix Bjært Hargreaves <dkfepaha@lego.com>
2024-08-27 13:47:36 +0200

    add code

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,17 +1,108 @@
 /**
- * Unit tests for the action's entrypoint, src/index.ts
+ * Unit tests for the action's main functionality, src/main.ts
+ *
+ * These should be run as if the action was called from a workflow.
+ * Specifically, the inputs listed in `action.yml` should be set as environment
+ * variables following the pattern `INPUT_<INPUT_NAME>`.
  */
 
-import * as main from '../src/main'
+import * as core from '@actions/core'
+import * as main from '../src/index'
 
-// Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation()
+// Mock the action's main function
+const runMock = jest.spyOn(main, 'run')
 
-describe('index', () => {
-  it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+// Mock the GitHub Actions core library
+let getInputMock: jest.SpiedFunction<typeof core.getInput>
+let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
+let addRawMock: jest.SpiedFunction<typeof core.summary.addRaw>
+let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
 
-    expect(runMock).toHaveBeenCalled()
+describe('action', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    addRawMock = jest.spyOn(core.summary, 'addRaw').mockImplementation()
+    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+  })
+
+  it('fails on empty json', async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'should-update-task-summary':
+          return 'true'
+        case 'plan-json':
+          return '{}'
+        default:
+          return ''
+      }
+    })
+
+    setOutputMock.mockImplementation((name, value) => {})
+    addRawMock.mockImplementation((markdown, addEOL) => {
+      return {} as any
+    })
+
+    main.run()
+
+    expect(setFailedMock).toHaveBeenCalledWith(
+      "Cannot read properties of undefined (reading 'filter')"
+    )
+  })
+
+  it('works', async () => {
+    const json = JSON.stringify(
+      {
+        resource_changes: [
+          {
+            change: {
+              actions: ['no-op'],
+              before: { foo: 'bar' },
+              after: { foo: 'baz' }
+            },
+            address: 'module.foo.bar'
+          }
+        ]
+      },
+      null,
+      0
+    )
+
+    console.log(json)
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'should-update-task-summary':
+          return 'true'
+        case 'plan-json':
+          return json
+        default:
+          return ''
+      }
+    })
+
+    setOutputMock.mockImplementation((name, value) => {})
+    addRawMock.mockImplementation((markdown, addEOL) => {
+      return {} as any
+    })
+
+    const expectedOutput = `## module.foo.bar
+
+\`\`\`diff
+ {
+-  foo: "bar"
++  foo: "baz"
+ }
+\`\`\`
+
+`
+    main.run()
+
+    expect(setOutputMock).toHaveBeenCalledWith(
+      'diff-as-markdown',
+      expectedOutput
+    )
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,43 @@
 /**
  * The entrypoint for the action.
  */
-import { run } from './main'
+import * as core from '@actions/core'
+import jsonDiff from 'json-diff'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
+export const run = () => {
+  try {
+    const planJson: any = JSON.parse(core.getInput('plan-json'))
+    const shouldUpdateTaskSummary: boolean =
+      core.getInput('should-update-task-summary') === 'true'
+
+    const allNonNoOpObjects = planJson.resource_changes.filter(
+      (resource: any) => resource.change.actions.includes('no-op')
+    )
+
+    var markdown = '' // To save you the trouble – yes `+=` is the fastest way to build strings: https://jsperf.app/join-concat/2 (and it's not even close)
+    for (const resource of allNonNoOpObjects) {
+      const diff = jsonDiff.diffString(
+        resource.change.before,
+        resource.change.after,
+        { color: false }
+      )
+
+      markdown += `## ${resource.address}\n\n`
+      markdown += '```diff\n'
+      markdown += diff
+      markdown += '```\n\n'
+
+      if (shouldUpdateTaskSummary) {
+        core.summary.addRaw(markdown)
+      }
+      console.log(`MARKDOWN: ${markdown}`)
+    }
+
+    core.setOutput('diff-as-markdown', markdown)
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
 run()
